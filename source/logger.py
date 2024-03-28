26ae4df447767b9a7420ee76e35c4639c35fc5a9
bhagyalaxmiai <161131605+bhagyalaxmiai@users.noreply.github.com>
2024-03-28 19:23:34 +0530

    Logger and exceptions added

--- source/logger.py
+++ source/logger.py
@@ -12,7 +12,8 @@ logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-#include the timestamp, line number, log level, and the message itself.
+
+# include the timestamp, line number, log level, and the message itself.
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d - %(levelname)s - %(message)s"
